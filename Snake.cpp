80606d820a120ad0ecde0b1f0a2ec2cc12919ae9
yasir <murdockbobbin@gmail.com>
2026-05-13 21:00:30 -0400

    partially rewrote snake class with circular buffer after blunder

---
+++ Snake.cpp
@@ -0,0 +1,67 @@
+#include "Snake.h"
+
+#include <stdexcept>
+
+Snake::Snake():
+    circular_buffer(nullptr),
+    direction_offset(0,0),
+    head_idx(0),
+    tail_idx(0),
+    snake_len(0),
+    buffer_size(0)
+    {
+    //constructor
+}
+Snake::~Snake(){
+    delete [] circular_buffer;
+}
+void Snake::build(int buffer_size){
+    circular_buffer = new glm::vec2[buffer_size];
+}
+int Snake::pushBack(glm::vec2 pos){
+    tail_idx = (tail_idx+1+buffer_size)%buffer_size;
+    circular_buffer[tail_idx] = pos;
+    snake_len++;
+}
+
+//getters:
+glm::vec2 Snake::getHeadPos(){
+    return circular_buffer[head_idx];
+}
+glm::vec2 Snake::getTailPos(){
+    return circular_buffer[tail_idx];
+}
+int Snake::getSnakeLen(){
+    return snake_len;
+}
+SnakeIterator Snake::getIterator(){
+    return SnakeIterator(circular_buffer,buffer_size,head_idx,snake_len);//copy elision
+}
+
+//movement:
+void Snake::changeDirection(Direction d){
+    switch (d)
+    {
+    case Direction::UP:
+        direction_offset.x = -1;
+        direction_offset.y = 0;
+        break;
+    case Direction::RIGHT:
+        direction_offset.x = 0;
+        direction_offset.y = 1;
+        break;
+    case Direction::DOWN:
+        direction_offset.x = 1;
+        direction_offset.y = 0;
+        break;
+    case Direction::LEFT:
+        direction_offset.x = 0;
+        direction_offset.y = -1;
+        break;
+    
+    default:
+        throw std::runtime_error("impossible direction");
+        break;
+    }
+}
+glm::vec2 moveOnce(); //return prev_tail
---
+++ Snake.h
@@ -0,0 +1,40 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include "glm/vec2.hpp"
+#include "Direction.h"
+#include "SnakeIterator.h"
+
+class Snake{
+    
+    public:
+    Snake();
+    ~Snake();
+    void build(int buffer_size);
+    int pushBack(glm::vec2 pos);
+
+    //getters:
+    glm::vec2 getHeadPos();
+    glm::vec2 getTailPos();
+    int getSnakeLen();
+    SnakeIterator getIterator();
+    
+
+    //movement:
+    void changeDirection(Direction d);
+    glm::vec2 moveOnce(); //return prev_tail
+    
+
+
+    private:
+    glm::vec2* circular_buffer;
+    glm::vec2 direction_offset;
+    int head_idx;
+    int tail_idx;
+    int snake_len;
+    int buffer_size;
+
+
+};
+
+#endif
