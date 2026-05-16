f797d95f4f164f736dce534fbc511a14642e2a0c
yasir <murdockbobbin@gmail.com>
2026-05-16 15:25:14 -0400

    now shows frame duration in ms

--- main.cpp
+++ main.cpp
@@ -24,27 +24,36 @@ int main(int argc, char * argv[])
 
     auto start_time=std::chrono::high_resolution_clock::now();
     double fps_sum = 0;
+    std::chrono::nanoseconds frame_time_sum = std::chrono::nanoseconds{0};
     int frame_count = 0;
     double average_fps = 0;
+    std::chrono::nanoseconds average_frame_time = std::chrono::nanoseconds{0};
     
     
     while(!game_over){
-        auto start_time=std::chrono::high_resolution_clock::now();
+        std::chrono::time_point start_time=std::chrono::steady_clock::now();
 
         input();
         game_over = game.gameLogic();
         std::stringstream& ss = draw(game,renderer);
         
         // std::this_thread::sleep_for(std::chrono::milliseconds(1000/fps));    
-        std::chrono::nanoseconds delta_time = std::chrono::high_resolution_clock::now()-start_time;
+        std::chrono::nanoseconds delta_time = std::chrono::steady_clock::now()-start_time;
+        frame_time_sum += delta_time;
 
         ss << "average fps: " 
             << average_fps
-            << std::endl;
+            << std::endl
+            << "average frame time: "
+            << std::chrono::duration_cast<std::chrono::milliseconds> (average_frame_time).count()
+            << " ms.";
         std::cout << ss.str();
 
         fps_sum += 1'000'000'000/ delta_time.count();
-        average_fps = fps_sum/++frame_count;
+        
+        frame_count++;
+        average_fps = fps_sum/frame_count;
+        average_frame_time = frame_time_sum/frame_count;
         if(frame_count > 30*10){
             exit(0);
         }
